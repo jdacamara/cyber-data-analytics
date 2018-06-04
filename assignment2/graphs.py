c00bd85d1e4e196b948084be94a49934e8dc2db2
J Da Camara <jehandacamara@hotmail.com>
2018-06-05 01:59:54 +0200

    Plot auto correlations

---
+++ assignment2/graphs.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+from matplotlib import pyplot
+from data import TEST_DATA, TRAINING_DATA_1
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.graphics.tsaplots import plot_pacf
+
+TRAINING_DATA_1["DATETIME"] = pd.to_datetime(TRAINING_DATA_1["DATETIME"])
+
+TRAINING_DATA_1.index = TRAINING_DATA_1["DATETIME"]
+del TRAINING_DATA_1["DATETIME"]
+
+lt_1 = pd.DataFrame(TRAINING_DATA_1["L_T1"])
+
+resampled_lt1 = lt_1.resample("H").mean()
+
+plot_pacf(resampled_lt1, lags = 40)
+plot_acf(resampled_lt1, lags = 40)
+
+pyplot.show()
