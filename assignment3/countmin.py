b3f1b11d75c3bf56610b0bcf20026eec7e83c944
J Da Camara <jehandacamara@hotmail.com>
2018-06-23 15:19:46 +0200

    Small changes for efficiency

---
+++ assignment3/countmin.py
@@ -0,0 +1,97 @@
+import numpy as np
+import mmh3
+import time
+import math
+
+from random import randint
+from parse_data import Packet, LINES
+
+class CountMin:
+
+	def __init__(self, w, d, max_seed = 1024):
+		self.w = w 
+		self.d = d
+		self.matrix = np.zeros((d, w))
+		self.get_seeds(d, max_seed)
+		self.count = 0		
+
+
+	def get_seeds(self, d, max_seed):
+		seeds  = []
+		while len(seeds) < self.d:
+			r = randint(0, max_seed)
+			if r not in seeds:
+				seeds.append(r)
+
+		self.seeds = seeds
+
+	def add_item(self, item):
+		self.count = self.count + 1 
+		for i in range(self.d):
+			seed  = self.seeds[i]
+			position = mmh3.hash(item, seed) % self.w
+			self.matrix[i][position] = self.matrix[i][position] + 1
+
+	def get_count(self,item):
+		counts = []
+		for i in range(self.d):
+			seed  = self.seeds[i]
+			position = mmh3.hash(item, seed) % self.w
+			value = self.matrix[i][position]
+			counts.append(value)
+
+		return min(counts)
+
+	def get_top_k(self, list_values, k = 10):
+		dic = {}
+
+		for item in list_values:
+			dic[item] = self.get_count(item)
+
+		return sorted(dic.items(), key=lambda x: x[1], reverse = True)[:k]
+
+def optimized_countmin(self, delta = 0.01, epsilon =  0.00001):
+	w = 2/epsilon
+	d = math.log(1/delta)
+
+	count_min = CountMin(w, d)
+	return count_min
+
+
+
+start = time.time()
+c = CountMin(100,10)
+
+amount_of_ip = 0
+list_of_ip = []
+
+lines = read_file("capture20110811.pcap.netflow.labeled")
+for l in lines[1:]:
+
+	p = Packet(l)
+	'''
+	if p.source_ip != "147.32.84.165":
+		continue
+	'''
+
+	ip = p.destination_ip
+
+	c.add_item(ip)
+	amount_of_ip = amount_of_ip +1 
+
+	if ip not in list_of_ip:
+		list_of_ip.append(ip)
+
+
+results = c.get_top_k(list_of_ip)
+
+print ("amount_of_ip = %s" %amount_of_ip)
+for ip, estimation in results:
+	percent = (estimation / amount_of_ip) *100
+
+	print("%s & %.1f & %s \\\\" %(ip, percent, estimation))
+
+
+
+print ('It took ', time.time() - start, ' seconds.')
+print ("This was for w = %s" %c.w)
---
+++ assignment3/parse_data.py
@@ -0,0 +1,68 @@
+from pandas import datetime
+
+def read_file(filename):
+	with open(filename) as f:
+		lines = f.readlines()
+
+	return lines
+
+def ip_and_port(ip_port):
+	temp = ip_port.split(":")
+	if len(temp) == 1:
+		 ip = temp[0]
+		 return ip
+	elif len(temp) == 2:
+		ip = temp[0]
+		port = temp[1]
+		return ip
+	elif len(temp) == 6 :
+		ip = ":".join(temp[:6])
+		return ip
+	elif len(temp) == 8 or len(temp) == 7:
+		#print(temp)
+		ip = ":".join(temp[:6])
+		return ip
+	else:
+		ip = ":".join(temp[:6])
+		return ip
+
+def parse_date(date):
+	date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S.%f')
+	return date
+		
+
+class Packet():
+
+	def __init__(self, line):
+		self.parse_line(line)
+
+	def parse_line(self,line):
+		data = line.split()
+		#print(len(data))
+		
+		self.date = parse_date(data[0] + " " + data[1])
+		self.duration = data[2]
+		self.protocol = data[3]
+		self.source_ip =ip_and_port(data[4])
+		self.destination_ip = ip_and_port(data[6])
+		self.flags = data[7]
+		self.tos = data[8]
+		self.packets = data[9]
+		self.bytes = data[10]
+		self.flows = data[11]
+		self.label = data[12]
+
+		if self.label != "Botnet" and self.label != "Background" and self.label != 'LEGITIMATE':
+			print (self.label)
+
+
+
+#LINES = read_file("capture20110811.pcap.netflow.labeled")
+
+'''
+packets = []
+for l in lines[1:]:
+	packets.append(Packet(l))
+
+#print((len(lines) -1) == len(packets))
+'''
